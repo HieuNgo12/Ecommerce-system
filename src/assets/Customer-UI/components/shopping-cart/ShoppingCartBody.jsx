295086409cdb2473121a39acdcbf768b21ea208d
Ngo The Hieu <cusaniv@gmail.com>
2024-08-16 09:45:42 +0700

    fixed bug

--- src/assets/Customer-UI/components/shopping-cart/ShoppingCartBody.jsx
+++ src/assets/Customer-UI/components/shopping-cart/ShoppingCartBody.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./ShoppingCartBody.css";
 import { Link } from "react-router-dom";
+import CartRow from "./CartRow";
 
 function ShoppingCartBody() {
+  const [subTotal,setSubTotal] = useState(0);
   const cartList = JSON?.parse(localStorage?.getItem("cartList"));
   let quantityCartList = {};
   let cartItemList = [];
@@ -48,41 +50,7 @@ function ShoppingCartBody() {
           <tbody>
             {cartItemList.map((cartItem) => {
               return (
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th
-                    scope="row"
-                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    <div className="flex">
-                      <div className="image">
-                        {cartItem[1][0]?.img ? (
-                          <img
-                            src={cartItem[1][0].img}
-                            style={{ width: "30px", height: "30px" }}
-                          />
-                        ) : null}
-                      </div>
-                      <div className="">{cartItem[0]}</div>
-                    </div>
-                  </th>
-                  <td class="px-6 py-4">
-                    {cartItem[1][0]?.price ? cartItem[1][0].price + " $" : null}
-                  </td>
-                  <td class="px-6 py-4">
-                    <input
-                      className="quantity"
-                      min="0"
-                      type="number"
-                      defaultValue={cartItem[1].length}
-                    />
-                  </td>
-                  <td class="px-6 py-4">
-                    {" "}
-                    {cartItem[1][0]?.price
-                      ? cartItem[1][0].price * cartItem[1].length + " $"
-                      : null}
-                  </td>
-                </tr>
+                <CartRow cartItem={cartItem}/>
               );
             })}
           </tbody>
